73209994ce6fe36b56617866225b3e64f3e45266
kumarikolagana <kumarikolaganaa@gmail.com>
2024-10-15 19:24:29 +0530

    this is my 6th commit

--- src/components/Generat.js
+++ src/components/Generat.js
@@ -10,8 +10,22 @@ import Typography from '@mui/material/Typography';
 
 
 import { styled } from '@mui/material/styles';
-
 import { ThemeProvider } from '@mui/material/styles';
+import EdgesensorHighRoundedIcon from '@mui/icons-material/EdgesensorHighRounded';
+
+
+const items = [
+
+  {
+      icon: <EdgesensorHighRoundedIcon />,
+      title: 'Generating mock data',
+      description:
+        'This item could provide information about the mobile app version of the product.',
+      imageLight: 'url("/static/images/templates/templates-images/mobile-light.png")',
+      imageDark: 'url("/static/images/templates/templates-images/mobile-dark.png")',
+    },
+
+]
 const Chip = styled(MuiChip)(({ theme }) => ({
   variants: [
     {
@@ -32,36 +46,95 @@ const Chip = styled(MuiChip)(({ theme }) => ({
   ],
 }));
 
-export default function Generat() {
+function MobileLayout({ selectedItemIndex, handleItemClick, selectedFeature }) {
+  if (!items[selectedItemIndex]) {
+    return null;
+  }
+
   return (
-    // <ThemeProvider
-    //   theme={{
-    //     palette: {
-    //       primary: {
-    //         main: '#007FFF',
-    //         dark: '#0066CC',
-    //         display: { xs: 'flex', sm: 'none' },
-    //       },
-    //     },
-    //   }}
-    // >
-     
-        <Box
+    <Box
+      sx={{
+        display: { xs: 'flex', sm: 'none' },
+        flexDirection: 'column',
+        gap: 2,
+      }}
+    >
+      <Box sx={{ display: 'flex', gap: 2, overflow: 'auto' }}>
+        {items.map(({ title }, index) => (
+          <Chip
+            size="medium"
+            key={index}
+            label={title}
+            onClick={() => handleItemClick(index)}
+            selected={selectedItemIndex === index}
+          />
+        ))}
+      </Box>
+      
+    </Box>
+  );
+}
+
+MobileLayout.propTypes = {
+  handleItemClick: PropTypes.func.isRequired,
+  selectedFeature: PropTypes.shape({
+    description: PropTypes.string.isRequired,
+    icon: PropTypes.element,
+    imageDark: PropTypes.string.isRequired,
+    imageLight: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  selectedItemIndex: PropTypes.number.isRequired,
+};
+
+
+export default function Features() {
+  const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
+
+  const handleItemClick = (index) => {
+    setSelectedItemIndex(index);
+  };
+
+const selectedFeature = items[selectedItemIndex];
+
+  return (
+    <Container id="features" sx={{ py: { xs: 8, sm: 16  } }}>
+      <Box sx={{ width: { sm: '100%', md: '60%' } }}>
+        <Typography
+          component="h2"
+          variant="h4"
+          gutterBottom
+          sx={{ color: 'text.primary' }}
+        >
+          Generating mock data
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ color: 'text.secondary', mb: { xs: 2, sm: 4 } }}
+        >
+          Provide a brief overview of the key features of the product. For example,
+          you could list the number of features, their types or benefits, and
+          add-ons.
+        </Typography>
+      </Box>
+      <Box
         sx={{
           display: 'flex',
-          flexDirection: { xs: 'column', md: 'row-reverse' },
-          gap: 2,
+          flexDirection: { xs: 'column', md: 'column' },
+         flexDirection:"row",
+         alignItems: 'left',
+          gap: 6,
         }}
       >
         <div>
           <Box
             sx={{
               display: { xs: 'none', sm: 'flex' },
-              flexDirection: 'column',
+              flexDirection: 'colum',
               gap: 2,
               height: '100%',
             }}
-          >
+           > 
             {items.map(({ icon, title, description }, index) => (
               <Box
                 key={index}
@@ -106,21 +179,20 @@ export default function Generat() {
               </Box>
             ))}
           </Box>
-        ))
-          {/* </Box>
-          <MobileLayout
-            selectedItemIndex={selectedItemIndex}
-            handleItemClick={handleItemClick}
-            selectedFeature={selectedFeature}
-          /> */}
-        </div>
+          
+        </div> 
         <Box
-          sx={{
-            display: { xs: 'none', sm: 'flex' },
-            width: { xs: '100%', md: '70%' },
-            height: 'var(--items-image-height)',
+  
+         sx={{
+          display: { xs: 'none', sm: 'flex' },
+          width: { xs: '100%', md: '70%' },
+          height: 'var(--items-image-height)',
+          // flexDirection: 'row-reverse',
           }}
-        >
+
+          >
+          
+
           <Card
             variant="outlined"
             sx={{
@@ -128,29 +200,35 @@ export default function Generat() {
               width: '100%',
               display: { xs: 'none', sm: 'flex' },
               pointerEvents: 'none',
+              flexDirection: 'row-reverse', 
             }}
-          >
+            >
+          
             <Box
               sx={(theme) => ({
                 m: 'auto',
                 width: 420,
                 height: 500,
+                // flexDirection: 'row-reverse', 
                 backgroundSize: 'contain',
                 backgroundImage: 'var(--items-imageLight)',
                 ...theme.applyStyles('dark', {
                   backgroundImage: 'var(--items-imageDark)',
                 }),
               })}
-              style={
-                items[selectedItemIndex]
-                  ? {
-                      '--items-imageLight': items[selectedItemIndex].imageLight,
-                      '--items-imageDark': items[selectedItemIndex].imageDark,
-                    }
-                  : {}
-              }
-            />
+              //   style={  
+              //      items[selectedItemIndex]
+              //       ? {
+              //           '--items-imageLight': items[selectedItemIndex].imageLight,
+              //        '--items-imageDark': items[selectedItemIndex].imageDark,
+              //        }
+              //      : {}
+              //  }
+            /> 
           </Card>
-        </Box>
+         </Box>
       </Box>
-  )}
+     
+    </Container>
+  );
+}

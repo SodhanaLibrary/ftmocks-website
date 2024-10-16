b5c319e18d7f61cb4d2069cc4e41b7c2d78cd8e3
Srinivas Dasari <srinivas.dasari@nilesecure.com>
2024-10-16 18:22:20 +0530

    initial changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MarketingPage from './MarketingPage';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MarketingPage />
+    <App />
   </React.StrictMode>
 );
 

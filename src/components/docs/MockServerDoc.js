497960f37d4f19002173d0cb4083b20ceec0496b
Srinivas Dasari <srinivas.dasari@nilesecure.com>
2024-11-02 19:38:52 +0530

    Updateds Docs

--- src/components/docs/MockServerDoc.js
+++ src/components/docs/MockServerDoc.js
@@ -7,83 +7,19 @@ const Installation = () => {
         <Box sx={{ padding: 4 }}>
           {/* Heading */}
           <Typography variant="h4" gutterBottom>
-            Getting Started
+            Mock Server
           </Typography>
     
-          {/* Prerequisites Section */}
-          <Typography variant="h5" gutterBottom>
-            Prerequisites
-          </Typography>
           <Typography variant="body1" gutterBottom>
-            To run this project, ensure you have the following installed on your machine:
+          Now, select a test case and start the mock server. The server will prioritize serving responses from the mock data of the selected test case. If a matching response isn't found in the test case data, it will fall back to the default mock data to ensure a complete response.
           </Typography>
-          <ul>
-            <li>
-              <Typography variant="body1">
-                <Link href="https://nodejs.org/" target="_blank" rel="noopener">
-                  Node.js
-                </Link>{' '}
-                (version 20 or later)
-              </Typography>
-            </li>
-            <li>
-              <Typography variant="body1">
-                <Link href="https://www.npmjs.com/" target="_blank" rel="noopener">
-                  npm
-                </Link>{' '}
-                (usually included with Node.js)
-              </Typography>
-            </li>
-          </ul>
-    
-          {/* Installation Section */}
-          <Typography variant="h5" gutterBottom>
-            Installation
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            1. Clone the repository:
-          </Typography>
-          <Box component="pre" sx={commonCodeStye}>
-            git clone https://github.com/SodhanaLibrary/ftmocks-server.git
-          </Box>
-          <Typography variant="body1" gutterBottom>
-            2. Navigate into the project directory:
-          </Typography>
-          <Box component="pre" sx={commonCodeStye}>
-            cd ftmocks-server
-          </Box>
-    
-          {/* Configuration Section */}
-          <Typography variant="h5" gutterBottom>
-            Configuration
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            1. Create a <code>my-project.env</code> file for your project.
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            2. Add the following environment variables in <code>my-project.env</code> in the project root:
-          </Typography>
-          <Box component="pre" sx={commonCodeStye}>
-            MOCK_DIR=/Users/srinivas.dasari/Documents/CursorWorkSpace/ftmocks-server/sample/my-project
-            <br />
-            PORT=5000
-            <br />
-            MOCK_DEFAULT_FILE=default.json
-            <br />
-            MOCK_DEFAULT_DIR=defaultMocks
-            <br />
-            MOCK_TEST_FILE=tests.json
-          </Box>
-    
-          {/* Running the Project Section */}
-          <Typography variant="h5" gutterBottom>
-            Running the Project
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            To start the project, use:
-          </Typography>
-          <Box component="pre" sx={commonCodeStye}>
-            npm start my-project
+          <Box sx={{p: 1, background: '#fff'}}>
+            <img
+              src="/static/mock_server.png"
+              alt="export HAR file"
+              loading="lazy"
+              width="100%"
+            />
           </Box>
         </Box>
   );
